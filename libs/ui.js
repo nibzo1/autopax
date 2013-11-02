45bf3199de709403c7744d1bd92458b63f7f04b6
Sharon Emmett <sharon.emmett@openjawtech.com>
2013-11-02 03:20:17 +0000

    tidied up ui methods

--- libs/ui.js
+++ libs/ui.js
@@ -1,14 +1,7 @@
 $(function() {
 
 	var max = 0;
-    $("label").each(function(){
-        if ($(this).width() > max)
-            max = $(this).width();    
-    });
-    $("label").width(max);
-		
-	$( "#accordion" ).accordion();
-	
+
 	var availableTags = [
 		"4111111111111111",
 		"4444333322221111",
@@ -17,73 +10,71 @@ $(function() {
 		"5454545454545454",
 		"36148900647913"
 	];
-	$( "#cardNumber" ).autocomplete({
+
+   $("label").width(max);
+
+	$("#tabs").tabs();
+
+    $("label").each(function(){
+        if ($(this).width() > max)
+            max = $(this).width();    
+    }); 
+		
+	$("#accordion").accordion();
+
+	$("#cardNumber").autocomplete({
 		source: availableTags
 	});
+		
+	$("#button").button();
 	
-
+	$("#radioset").buttonset();			
 	
-	$( "#button" ).button();
-	$( "#radioset" ).buttonset();
-	
-
-	
-	$( "#tabs" ).tabs();
-	
-
-	
-	$( "#dialog" ).dialog({
+	$("#dialog").dialog({
 		autoOpen: false,
 		width: 400,
 		buttons: [
 			{
 				text: "Ok",
 				click: function() {
-					$( this ).dialog( "close" );
+					$(this).dialog("close");
 				}
 			},
 			{
 				text: "Cancel",
 				click: function() {
-					$( this ).dialog( "close" );
+					$(this).dialog("close");
 				}
 			}
 		]
 	});
 
 	// Link to open the dialog
-	$( "#dialog-link" ).click(function( event ) {
-		$( "#dialog" ).dialog( "open" );
+	$("#dialog-link").click(function(event) {
+		$("#dialog").dialog("open");
 		event.preventDefault();
 	});
-	
-
-	
-	$( "#datepicker" ).datepicker({
+		
+	$("#datepicker").datepicker({
 		inline: true
 	});
 	
-
-	
-	$( "#slider" ).slider({
+	$("#slider").slider({
 		range: true,
-		values: [ 17, 67 ]
+		values: [17, 67]
 	});
 	
-
-	
-	$( "#progressbar" ).progressbar({
+	$("#progressbar").progressbar({
 		value: 20
 	});
 	
-
 	// Hover states on the static widgets
 	$( "#dialog-link, #icons li" ).hover(
 		function() {
-			$( this ).addClass( "ui-state-hover" );
+			$(this).addClass("ui-state-hover");
 		},
 		function() {
-			$( this ).removeClass( "ui-state-hover" );
+			$(this).removeClass("ui-state-hover");
 		}
 	);
 });
