eba3927c8e2264db00f2615b90b933edbfa6d67c
Sharon Emmett <sharon.emmett@openjawtech.com>
2013-11-02 03:23:28 +0000

    initial commit of context menu config

---
+++ autopax/contextMenus.js
@@ -0,0 +1,29 @@
+
+//context menu
+var ibe = chrome.contextMenus.create({
+	"title": "Enable ibe", "id" : "ibe", "type": "checkbox", "checked" : isChecked("ibeenabled"), "onclick":checkboxOnClick
+});
+var grp = chrome.contextMenus.create({
+	"title": "Enable group", "id" : "grp", "type": "checkbox", "checked" :  isChecked("grpenabled"),  "onclick":checkboxOnClick
+});
+var nameFirm = chrome.contextMenus.create({
+	"title": "Enable NameFirming", "id" : "nf", "type": "checkbox", "checked" : isChecked("nfenabled"), "onclick":checkboxOnClick
+});
+var exp = chrome.contextMenus.create({
+	"title": "Enable Express", "id" : "exp", "type": "checkbox", "checked" : isChecked("expenabled"), "onclick":checkboxOnClick
+});
+var crd = chrome.contextMenus.create({
+	"title": "Enable Credit Card", "id" : "crd", "type": "checkbox", "checked" : isChecked("crdenabled"), "onclick":checkboxOnClick
+});
+var nump = chrome.contextMenus.create({
+	"title": "Enable Number Plate", "id" : "nump", "type": "checkbox", "checked" : isChecked("numpenabled"), "onclick":checkboxOnClick
+});
+var tac = chrome.contextMenus.create({
+	"title": "Enable T&Cs", "id" : "tac", "type": "checkbox", "checked" : isChecked("tacenabled"), "onclick":checkboxOnClick
+});
+var allpax = chrome.contextMenus.create({
+	"title": "Enable All Pax", "id" : "allpax", "type": "checkbox", "checked" : isChecked("allpaxenabled"), "onclick":checkboxOnClick
+});
+var opts = chrome.contextMenus.create({
+	"title": "Open Config", "id" : "config", "onclick":openConfig
+});
