893056d76a2094db34fae773fe8d326f8c7513f0
git <andyhowell8@gmail.com>
2014-04-09 11:51:15 +0100

    dont autopaxify fileds in ibe if they have values i.e. disabled

--- autopax/contentScripts/ibePax.js
+++ autopax/contentScripts/ibePax.js
@@ -5,112 +5,134 @@ chrome.extension.sendRequest({method: "getLocalStorage", key: "ibeenabled"}, fun
 	}
 });
 
-chrome.extension.onRequest.addListener(function(request, sender, sendResponse){ 
-	if(request == "ibeenabled"){ 			
+chrome.extension.onRequest.addListener(function(request, sender, sendResponse){
+	if(request == "ibeenabled"){
 		doPax(firstName,surname,email,phone,contactAddress1,contactCity,billPostcode,countryIDX);
-	} 
-}); 
+	}
+});
 
-chrome.extension.onRequest.addListener(function(request, sender, sendResponse){ 
-	if(request == "ibedisabled"){ 				
+chrome.extension.onRequest.addListener(function(request, sender, sendResponse){
+	if(request == "ibedisabled"){
 		doPax("","","","","","","","");
-	} 
-}); 
+	}
+});
 
 function doPax(firstName,surname,email,phone,contactAddress1,contactCity,billPostcode,countryIDX){
 	var suffix = function (idx) {
 		return String.fromCharCode(idx + 65);
 	};
-	$('select.gender').each(function (idx, ele) {		
-		if(firstName===''){
-			ele.selectedIndex = 0;
+	$('select.gender').each(function (idx, ele) {
+		if(!$(ele).is(':disabled') ){
+			if(firstName===''){
+				ele.selectedIndex = 0;
+			}
+			else{
+				ele.selectedIndex = 1;
+			}
 		}
-		else{
-			ele.selectedIndex = 1;
-		}		
 	});
 	$('select.titlelarge').each(function (idx, ele) {
-		if(firstName===''){
-			ele.selectedIndex = 0;
+		if(!$(ele).is(':disabled') ){
+			if(firstName===''){
+				ele.selectedIndex = 0;
+			}
+			else{
+				ele.selectedIndex = 1;
+			}
 		}
-		else{
-			ele.selectedIndex = 1;
-		}				
 	});
 	$('input.xsmall.name').each(function (idx, ele) {
 		ele.focus();
-		if(firstName===''){
-			ele.value = firstName;			
+		if(!$(ele).is(':disabled') ){
+			if(firstName===''){
+				ele.value = firstName;
+			}
+			else{
+				ele.value = firstName + suffix(idx);
+			}
 		}
-		else{
-			ele.value = firstName + suffix(idx);
-		}		
-		ele.blur()
+		ele.blur();
 	});
 	$('input.small.name').each(function (idx, ele) {
-		ele.focus();		
-		if(surname===''){
-			ele.value = surname;			
+		ele.focus();
+		if(!$(ele).is(':disabled') ){
+			if(surname===''){
+				ele.value = surname;
+			}
+			else{
+				ele.value = surname + suffix(idx);
+			}
 		}
-		else{
-			ele.value = surname + suffix(idx);
-		}
-		ele.blur()
+		ele.blur();
 	});
-	$('select.age').each(function (idx, ele) {		
-		if(firstName===''){
-			ele.selectedIndex = 0;
+	$('select.age').each(function (idx, ele) {
+		if(!$(ele).is(':disabled') ){
+			if(firstName===''){
+				ele.selectedIndex = 0;
+			}
+			else{
+				ele.selectedIndex = 2
+			}
 		}
-		else{
-			ele.selectedIndex = 2
-		}		
 	});
-	$('select.medium').each(function (idx, ele) {		
-		if(countryIDX == 0){
-			ele.selectedIndex = 0; 			
+	$('select.medium').each(function (idx, ele) {
+		if(!$(ele).is(':disabled') ){
+			if(countryIDX == 0){
+				ele.selectedIndex = 0;
+			}
+			else{
+				ele.selectedIndex = countryIDX;
+			}
 		}
-		else{
-			ele.selectedIndex = countryIDX; 
-		}		
 	});
-	$('#email1, #email2').each(function (idx, ele) {		
-		if(surname===''){
-			ele.value = '';			
-		}
-		else{
-			ele.value = email;
+	$('#email1, #email2').each(function (idx, ele) {
+		if(!$(ele).is(':disabled') ){
+			if(surname===''){
+				ele.value = '';
+			}
+			else{
+				ele.value = email;
+			}
 		}
 	});
 	$('#phone1').each(function (idx, ele) {
-		if(phone===''){
-			ele.value = '';			
+		if(!$(ele).is(':disabled') ){
+			if(phone===''){
+				ele.value = '';
+			}
+			else{
+				ele.value = phone;
+			}
 		}
-		else{
-			ele.value = phone;
-		}		
 	});
-	$('#contact-address1').each(function (idx, ele) {		
-		if(contactAddress1===''){
-			ele.value = '';			
+	$('#contact-address1').each(function (idx, ele) {
+		if(!$(ele).is(':disabled') ){
+			if(contactAddress1===''){
+				ele.value = '';
+			}
+			else{
+				ele.value = contactAddress1;
+			}
 		}
-		else{
-			ele.value = contactAddress1;
-		}		
 	});
-	$('#contact-city').each(function (idx, ele) {		
-		if(contactCity===''){
-			ele.value = '';			
+	$('#contact-city').each(function (idx, ele) {
+		if(!$(ele).is(':disabled') ){
+			if(contactCity===''){
+				ele.value = '';
+			}
+			else{
+				ele.value = contactCity;
+			}
 		}
-		else{
-			ele.value = contactCity;
-		}		
 	});
-	$('#bill-postcode').each(function (idx, ele) {		
-		if(billPostcode===''){
-			ele.value = '';			
+	$('#bill-postcode').each(function (idx, ele) {
+		if(!$(ele).is(':disabled') ){
+			if(billPostcode===''){
+				ele.value = '';
+			}
+			else{
+				ele.value = billPostcode;
+			}
 		}
-		else{
-			ele.value = billPostcode;
-		}		
 	});
 }
