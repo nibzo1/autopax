93973ec31f5c2d36e4f6b971d388ef2b721af52e
git <andyhowell8@gmail.com>
2014-06-17 12:16:53 +0100

    updating some spacing

--- app/background.js
+++ app/background.js
@@ -1,6 +1,5 @@
 //listen for request from script
 chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
-
 	if(request.method === "getLocalStorage"){
 		sendResponse({data: localStorage[request.key]});
 	}
